ec23342a15b882b80d9fc114e39ca98e3bc6d398
Ryohei Nagasawa <49315610+ryoheinan@users.noreply.github.com>
2024-10-14 17:31:37 +0900

    fix: Remove `if (import.meta.main)` to avoid bug with pnpm

--- src/main.ts
+++ src/main.ts
@@ -39,40 +39,38 @@ export async function generateToken(secret: string, payload?: TokenPayload) {
   return jwt;
 }
 
-if (import.meta.main) {
-  try {
-    const args = parseArgs(Deno.args);
+try {
+  const args = parseArgs(Deno.args);
 
-    const envFilePath = args["env-file"];
-    if (typeof envFilePath !== "undefined" && typeof envFilePath !== "string") {
-      throw new Error("env-file is invalid");
-    }
-
-    await loadEnv(envFilePath);
-
-    const secret = Deno.env.get(TOKEN_SECRET_NAME);
-    if (!secret) {
-      throw new Error(`${TOKEN_SECRET_NAME} is not set in env file`);
-    }
-
-    const serviceName = args["service-name"];
-    if (typeof serviceName === "undefined") {
-      throw new Error("service-name is required");
-    }
-    if (typeof serviceName !== "string") {
-      throw new Error("service-name is invalid");
-    }
-
-    const token = await generateToken(secret, {
-      service_name: serviceName,
-    });
-    console.info(`token: ${token}`);
-  } catch (e) {
-    if (e instanceof Error) {
-      console.error("Failed to create token:", e.message);
-    } else {
-      console.error(e);
-    }
-    Deno.exit(1);
+  const envFilePath = args["env-file"];
+  if (typeof envFilePath !== "undefined" && typeof envFilePath !== "string") {
+    throw new Error("env-file is invalid");
   }
+
+  await loadEnv(envFilePath);
+
+  const secret = Deno.env.get(TOKEN_SECRET_NAME);
+  if (!secret) {
+    throw new Error(`${TOKEN_SECRET_NAME} is not set in env file`);
+  }
+
+  const serviceName = args["service-name"];
+  if (typeof serviceName === "undefined") {
+    throw new Error("service-name is required");
+  }
+  if (typeof serviceName !== "string") {
+    throw new Error("service-name is invalid");
+  }
+
+  const token = await generateToken(secret, {
+    service_name: serviceName,
+  });
+  console.info(`token: ${token}`);
+} catch (e) {
+  if (e instanceof Error) {
+    console.error("Failed to create token:", e.message);
+  } else {
+    console.error(e);
+  }
+  Deno.exit(1);
 }
