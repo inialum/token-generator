30d953f5a5a81e9da41359fa71ff79bf94be07b6
Ryohei Nagasawa <49315610+ryoheinan@users.noreply.github.com>
2024-10-14 22:27:14 +0900

    chore: Refactor test

--- src/utils_test.ts
+++ src/utils_test.ts
@@ -1,14 +1,14 @@
 import { assertEquals, assertRejects } from "jsr:@std/assert";
+import { fromFileUrl } from "jsr:@std/path";
 import { loadEnv } from "./utils.ts";
-import * as path from "jsr:@std/path";
 
 Deno.test({
   name: "Should load env file with valid path",
   fn: async () => {
-    const testdataPath = path.fromFileUrl(
+    const testdataPath = fromFileUrl(
       new URL("./testdata/env.txt", import.meta.url),
     );
-    await loadEnv(path.resolve(testdataPath));
+    await loadEnv(testdataPath);
     const secret = Deno.env.get("INIALUM_SERVICE_TOKEN_SECRET");
 
     assertEquals(secret, "secret!");
@@ -18,7 +18,7 @@ Deno.test({
 Deno.test({
   name: "Should throw error when env file is not found",
   fn: () => {
-    const testdataPath = path.fromFileUrl(
+    const testdataPath = fromFileUrl(
       new URL("./testdata/invalid_path.txt", import.meta.url),
     );
     assertRejects(
